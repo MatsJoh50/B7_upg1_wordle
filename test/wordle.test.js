4fb5233a81c8924a7ec83fc084b011e376df3e8d
Mats Johansson <matjoh50@gmail.com>
2024-04-09 15:41:14 +0200

    translated comments to english

--- test/wordle.test.js
+++ test/wordle.test.js
@@ -1,7 +1,8 @@
 const wordle = require('../wordle.js');
 
-//Inleder med att testa att allt blir godkänt
-//Fungerar inledningsvis som ett "fail" test för att sedan bli correct.
+
+//First test to see if function will run and fail.
+//Next part of this test is to make it Succeed by "guessing" the correct word and getting "Correct" in return.
 test('Test to see if all is Correct: Word: hej, Guess, hej', () => {
 
     const result = wordle('hej', 'hej')
@@ -12,7 +13,8 @@ test('Test to see if all is Correct: Word: hej, Guess, hej', () => {
 })
 
 
-//Fortsätter med att testa om 'incorrect' fungerar
+
+//Now we implement the 'Incorrect' Value on one single letter for better controll of enviroment.
 test('Simple test for Correct/Incorrect: Word: cow, Guess: wow', () => {
 
 const result = wordle('cow', 'wow');
@@ -22,8 +24,7 @@ const result = wordle('cow', 'wow');
     expect(result[2].result).toEqual('Correct');
 });
 
-
-//Därefter lägger vi till enstaka 'misplaced'
+//When 'Incorrect' funcionallity works, we add 'Misplaced' on a single letter for easy control.
 test('More advanced to see if "a" and "l" get Correct status: Word: cykla, Guess: hallå', () => {
 
     const result = wordle('cykla', 'hallå');
@@ -35,7 +36,7 @@ test('More advanced to see if "a" and "l" get Correct status: Word: cykla, Guess
     expect(result[4].result).toEqual('Incorrect');
 })
 
-//Slutligen ett aningen med "avancerat" test för att testa fler återkommande bokstäver än 2st.
+//Finnaly we take a jibberish setup of letters to confirm that multiple instanses of each status('Correct', 'Incorrect', 'Misplaced') works correctly
 test('Test to see if words with 3x letter is Correct', () => {
 const result = wordle('eeesdfk', 'esefiee');
     expect(result[0].result).toEqual('Correct')
